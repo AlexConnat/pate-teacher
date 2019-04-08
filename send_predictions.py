592bc414e5c487cb9bc73f3a8b123709e03047a0
AlexConnat <alexandre.connat@epfl.ch>
2019-04-08 06:58:52 +0200

    More flexible command line arguments

--- send_predictions.py
+++ send_predictions.py
@@ -1,24 +1,30 @@
 #!/usr/bin/python
 
 import socket
+import sys
+
+if len(sys.argv) != 4:
+    print("Usage: %s server_ip server_port prediction_file" % (sys.argv[0])) # MUST BE A VALID IP AND VALID PORT
+    exit(1)
 
 CHUNK_SIZE = 8 * 1024
-SERVER_IP = "10.10.55.100"
-SERVER_PORT = 1337
+SERVER_IP = sys.argv[1]          # "10.10.55.100"
+SERVER_PORT = int(sys.argv[2])   # 1337
 
 sock = socket.socket()
 sock.connect( (SERVER_IP, SERVER_PORT) )
 
 print("[*] Connected to " + SERVER_IP + "\n")
 
-filename = "checkpoints/predictions.npy"
+#filename = "checkpoints/predictions.npy"
+filename = sys.argv[3]
 with open(filename, "rb") as f:
 	
-	data = f.read(CHUNK_SIZE)
-	while data:
-		sock.send(data)
-	    	data = f.read(CHUNK_SIZE)
+    data = f.read(CHUNK_SIZE)
+    while data:
+        sock.send(data)
+        data = f.read(CHUNK_SIZE)
 	
-	print("Sent data!\n")
-
-	sock.close()
+    print("Sent predictions!\n")
+    
+    sock.close()
